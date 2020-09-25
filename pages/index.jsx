70ed54eb36f3413d13637cdac936cc690ca6374a
mchlbataller <mchlbataller@gmail.com>
2020-09-25 12:07:03 +0800

    Boilerplate setup

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import {CssBaseline} from '@material-ui/core'
+export default function Home() {
+    return (
+        <>
+            <CssBaseline />
+            <h1>Testing</h1>
+        </>
+    )
+}
